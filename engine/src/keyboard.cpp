c9db1168fd56a90204924bce438525fb1bb6c126
Cosmodash <theo.github@bedafamily.com>
2021-02-24 20:27:23 -0800

    keyboard class now uses windows calls

--- engine/src/keyboard.cpp
+++ engine/src/keyboard.cpp
@@ -1,7 +1,10 @@
 #include "keyboard.h"
+#include "buffer.h"
+#include <cassert>
+#include <memory>
 
 #ifdef FEENGINE_WINDOWS
-#include <conio.h>
+#include <Windows.h>
 #endif
 
 namespace fe_engine {
@@ -10,13 +13,22 @@ namespace fe_engine {
 
 	void keyboard::update()
 	{
-		m_input.clear();
+		this->m_input.clear();
 #ifdef FEENGINE_WINDOWS
-		// TODO: Switch to Peek/ReadConsoleInput on windows as we can 
-		// do a lot more with those. This is simpler but deprecated.
-		while (_kbhit()) {
-			auto ch = _getch();
-			this->m_input.push_back(ch);
+		HANDLE stdinput = GetStdHandle(STD_INPUT_HANDLE);
+		DWORD number_of_events, events_read;
+		assert(GetNumberOfConsoleInputEvents(stdinput, &number_of_events));
+		std::unique_ptr<util::buffer> buf(new util::buffer(number_of_events * sizeof(INPUT_RECORD)));
+		assert(ReadConsoleInputA(stdinput, util::buffer_cast<INPUT_RECORD>(buf.get()), number_of_events, &events_read));
+		for (int i = 0; i < events_read; i++) {
+			INPUT_RECORD* ptr = util::buffer_cast<INPUT_RECORD>(buf.get());
+			INPUT_RECORD event = ptr[i];
+			if (event.EventType == KEY_EVENT) {
+				if ((event.Event.KeyEvent.wRepeatCount == 1) && event.Event.KeyEvent.bKeyDown) {
+					char c = event.Event.KeyEvent.uChar.AsciiChar;
+					this->m_input.push_back(c);
+				}
+			}
 		}
 #endif
 	}
