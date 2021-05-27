253a9980e971657f09286b619a0b15fcab8a9b51
Cosmodash <theo.github@bedafamily.com>
2021-05-26 17:04:42 -0700

    add newtonsoft.json dependency

--- rewrite.lua
+++ rewrite.lua
@@ -20,11 +20,25 @@ newoption {
     description = "The path to the Mono library directory",
     default = (default_mono_libdir)
 }
+newoption {
+    trigger = "cs-version",
+    value = "VER",
+    description = "C# version to compile code as",
+    default = "9.0"
+}
+newoption {
+    trigger = "framework-version",
+    value = "VER",
+    description = ".NET Framework version to use",
+    default = "2.0"
+}
 includedirs_table = {}
 libdirs_table = {}
 includedirs_table["mono"] = _OPTIONS["mono-include"]
 includedirs_table["cxxopts"] = "vendor/submodules/cxxopts/include"
 libdirs_table["mono"] = _OPTIONS["mono-libdir"]
+cs_version = _OPTIONS["cs-version"]
+dotnet_framework_version = _OPTIONS["framework-version"]
 workspace "fe_engine-rewrite"
     architecture (architecture_)
     targetdir "bin"
@@ -59,16 +73,73 @@ workspace "fe_engine-rewrite"
         }
         optimize "on"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+group "dependencies"
+project "Newtonsoft.Json"
+    location "rewrite/dependencies"
+    kind "SharedLib"
+    language "C#"
+    csversion (cs_version)
+    framework (dotnet_framework_version)
+    targetdir ("bin/" .. outputdir .. "/rewrite/dependencies/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/rewrite/dependencies/%{prj.name}")
+    files {
+        "vendor/submodules/%{prj.name}/Src/%{prj.name}/**.cs"
+    }
+    excludes {
+        "vendor/submodules/%{prj.name}/Src/%{prj.name}/obj/**.cs"
+    }
+    links {
+        "System.ComponentModel",
+        "System.Xml",
+        "System",
+        "System.Text.RegularExpressions",
+        "System.Core",
+        "System.Data"
+    }
+    filter "options:framework-version=2.0"
+        defines {
+            "NET20",
+            "HAVE_ADO_NET",
+            "HAVE_APP_DOMAIN",
+            "HAVE_BINARY_FORMATTER",
+            "HAVE_BINARY_SERIALIZATION",
+            "HAVE_BINARY_EXCEPTION_SERIALIZATION",
+            "HAVE_CAS",
+            "HAVE_CHAR_TO_LOWER_WITH_CULTURE",
+            "HAVE_CHAR_TO_STRING_WITH_CULTURE",
+            "HAVE_COM_ATTRIBUTES",
+            "HAVE_COMPONENT_MODEL",
+            "HAVE_DB_NULL_TYPE_CODE",
+            "HAVE_EMPTY_TYPES",
+            "HAVE_FAST_REVERSE",
+            "HAVE_FULL_REFLECTION",
+            "HAVE_ICLONEABLE",
+            "HAVE_ICONVERTIBLE",
+            "HAVE_MEMORY_BARRIER",
+            "HAVE_NON_SERIALIZED_ATTRIBUTE",
+            "HAVE_REFLECTION_EMIT",
+            "HAVE_STREAM_READER_WRITER_CLOSE",
+            "HAVE_TRACE_WRITER",
+            "HAVE_TYPE_DESCRIPTOR",
+            "HAVE_UNICODE_SURROGATE_DETECTION",
+            "HAVE_XML_DOCUMENT",
+            "HAVE_XML_DOCUMENT_TYPE"
+        }
+group ""
 group "engine"
 project "FEEngine"
     location "rewrite/src/FEEngine"
     kind "SharedLib"
     language "C#"
-    clr "unsafe"
+    csversion (cs_version)
+    framework (dotnet_framework_version)
     targetdir ("bin/" .. outputdir .. "/rewrite/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/rewrite/%{prj.name}")
     files {
         "rewrite/src/%{prj.name}/**.cs"
+    }
+    links {
+        "Newtonsoft.Json"
     }
 project "host"
     location "rewrite/src/host"
@@ -137,6 +208,8 @@ project "ExampleGame"
     location "rewrite/examples/ExampleGame"
     kind "SharedLib"
     language "C#"
+    csversion (cs_version)
+    framework (dotnet_framework_version)
     targetdir ("bin/" .. outputdir .. "/rewrite/examples/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/rewrite/examples/%{prj.name}")
     files {
--- rewrite/src/FEEngine/Registry.cs
+++ rewrite/src/FEEngine/Registry.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FEEngine
 {
