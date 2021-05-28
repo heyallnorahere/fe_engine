e5c6ee4205c09011b7fa44b2a7c7d67ad9741039
Cosmodash <theo.github@bedafamily.com>
2021-05-27 20:53:17 -0700

    add newtonsoft.json.schema dependency

--- premake/mono.lua
+++ premake/mono.lua
@@ -1,21 +1,28 @@
 local function get_windows_path_stub(architecture)
     local path = "C:/Program Files"
-    if (architecture == "x86" or architecture == "x32") then
+    if architecture == "x86" or architecture == "x32" then
         path = path .. " (x86)"
     end
     return path
+end
+local function get_unix_path_stub()
+    local path = "/usr"
+    if os.istarget("macosx") then
+        path = path .. "/local"
+    end
+    return path;
 end
 function determine_mono_include(architecture)
     if os.istarget("windows") then
         return get_windows_path_stub(architecture) .. "/Mono/include/mono-2.0"
     else
-        return "/usr/local/include/mono-2.0"
+        return get_unix_path_stub() .. "/include/mono-2.0"
     end
 end
 function determine_mono_libdir(architecture)
-    if (os.istarget("windows")) then
+    if os.istarget("windows") then
         return get_windows_path_stub(architecture) .. "/Mono/lib"
     else
-        return "/usr/local/lib"
+        return get_unix_path_stub() .. "/lib"
     end
 end
--- rewrite.lua
+++ rewrite.lua
@@ -30,7 +30,7 @@ newoption {
     trigger = "framework-version",
     value = "VER",
     description = ".NET Framework version to use",
-    default = "2.0"
+    default = "3.5"
 }
 includedirs_table = {}
 libdirs_table = {}
@@ -39,7 +39,7 @@ includedirs_table["cxxopts"] = "vendor/submodules/cxxopts/include"
 libdirs_table["mono"] = _OPTIONS["mono-libdir"]
 cs_version = _OPTIONS["cs-version"]
 dotnet_framework_version = _OPTIONS["framework-version"]
-dotnet_assembly_path = "%{libdirs_table.mono}/mono/%{dotnet_framework_version}-api"
+dotnet_assembly_path = "%{libdirs_table.mono}/mono"
 workspace "fe_engine-rewrite"
     architecture (architecture_)
     targetdir "bin"
@@ -92,8 +92,47 @@ project "Newtonsoft.Json"
     links {
         "System",
         "System.Data",
-        "System.Xml"
+        "System.Runtime.Serialization",
+        "System.Xml",
+        "System.Xml.Linq"
     }
+    filter "options:framework-version=3.5"
+        defines {
+            "NET35",
+            "HAVE_ADO_NET",
+            "HAVE_APP_DOMAIN",
+            "HAVE_BINARY_FORMATTER",
+            "HAVE_BINARY_SERIALIZATION",
+            "HAVE_BINARY_EXCEPTION_SERIALIZATION",
+            "HAVE_CAS",
+            "HAVE_CHAR_TO_STRING_WITH_CULTURE",
+            "HAVE_CHAR_TO_LOWER_WITH_CULTURE",
+            "HAVE_COM_ATTRIBUTES",
+            "HAVE_COMPONENT_MODEL",
+            "HAVE_DATA_CONTRACTS",
+            "HAVE_DATE_TIME_OFFSET",
+            "HAVE_DB_NULL_TYPE_CODE",
+            "HAVE_EMPTY_TYPES",
+            "HAVE_ENTITY_FRAMEWORK",
+            "HAVE_FAST_REVERSE",
+            "HAVE_FULL_REFLECTION",
+            "HAVE_HASH_SET",
+            "HAVE_ICLONEABLE",
+            "HAVE_ICONVERTIBLE",
+            "HAVE_INOTIFY_PROPERTY_CHANGING",
+            "HAVE_LINQ",
+            "HAVE_MEMORY_BARRIER",
+            "HAVE_NON_SERIALIZED_ATTRIBUTE",
+            "HAVE_REFLECTION_EMIT",
+            "HAVE_STREAM_READER_WRITER_CLOSE",
+            "HAVE_TIME_ZONE_INFO",
+            "HAVE_TRACE_WRITER",
+            "HAVE_TYPE_DESCRIPTOR",
+            "HAVE_UNICODE_SURROGATE_DETECTION",
+            "HAVE_XLINQ",
+            "HAVE_XML_DOCUMENT",
+            "HAVE_XML_DOCUMENT_TYPE"
+        }
     filter "options:framework-version=2.0"
         defines {
             "NET20",
@@ -123,6 +162,33 @@ project "Newtonsoft.Json"
             "HAVE_XML_DOCUMENT",
             "HAVE_XML_DOCUMENT_TYPE"
         }
+project "Newtonsoft.Json.Schema"
+    location "rewrite/dependencies"
+    kind "SharedLib"
+    language "C#"
+    csversion (cs_version)
+    framework (dotnet_framework_version)
+    targetdir ("bin/" .. outputdir .. "/rewrite/dependencies/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/rewrite/dependencies/%{prj.name}")
+    files {
+        "vendor/submodules/%{prj.name}/Src/%{prj.name}/**.cs"
+    }
+    excludes {
+        "vendor/submodules/%{prj.name}/Src/%{prj.name}/obj/**.cs"
+    }
+    links {
+        "Newtonsoft.Json",
+        "System",
+        "System.Data",
+        "System.Runtime.Serialization"
+    }
+    filter "options:framework-version=3.5"
+        defines {
+            "HAVE_ADO_NET",
+            "HAVE_DATE_TIME_OFFSET",
+            "HAVE_FULL_REFLECTION",
+            "NET35"
+        }
 group ""
 group "engine"
 project "FEEngine"
@@ -135,10 +201,12 @@ project "FEEngine"
     targetdir ("bin/" .. outputdir .. "/rewrite/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/rewrite/%{prj.name}")
     files {
-        "rewrite/src/%{prj.name}/**.cs"
+        "rewrite/src/%{prj.name}/**.cs",
+        _SCRIPT
     }
     links {
-        "Newtonsoft.Json"
+        "Newtonsoft.Json",
+        "Newtonsoft.Json.Schema"
     }
 project "host"
     location "rewrite/src/host"
@@ -151,8 +219,7 @@ project "host"
     objdir ("bin-int/" .. outputdir .. "/rewrite/%{prj.name}")
     files {
         "rewrite/src/%{prj.name}/**.cpp",
-        "rewrite/src/%{prj.name}/**.h",
-        _SCRIPT
+        "rewrite/src/%{prj.name}/**.h"
     }
     includedirs {
         "%{includedirs_table.mono}",
@@ -168,14 +235,17 @@ project "host"
         '{MOVE} "%{cfg.targetdir}/FEEngine.dll" "."',
         '{MOVE} "%{cfg.targetdir}/ExampleGame.exe" "."',
         '{MOVE} "%{cfg.targetdir}/Newtonsoft.Json.dll" "."',
-        '{COPY} "%{dotnet_assembly_path}/System.dll" "."',
-        '{COPY} "%{dotnet_assembly_path}/System.Data.dll" "."',
-        '{COPY} "%{dotnet_assembly_path}/System.Xml.dll" "."',
+        '{COPY} "%{dotnet_assembly_path}/2.0-api/System.dll" "."',
+        '{COPY} "%{dotnet_assembly_path}/2.0-api/System.Data.dll" "."',
+        '{COPY} "%{dotnet_assembly_path}/2.0-api/System.Runtime.Serialization.dll" "."',
+        '{COPY} "%{dotnet_assembly_path}/2.0-api/System.Xml.dll" "."',
+        '{COPY} "%{dotnet_assembly_path}/2.0-api/System.Xml.Linq.dll" "."'
     }
     links {
         "FEEngine",
         "ExampleGame",
-        "Newtonsoft.Json"
+        "Newtonsoft.Json",
+        "Newtonsoft.Json.Schema"
     }
     filter "configurations:Debug"
         targetsuffix "-d"
