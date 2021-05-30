8486bd20e82457211f6f005059a88115c9cdf48d
Cosmodash <theo.github@bedafamily.com>
2021-05-30 16:28:12 -0700

    delete rewrite for native branch

--- premake/dependencies/premake5.lua
+++
@@ -1,4 +0,0 @@
-group "dependencies"
-include "newtonsoft.json.lua"
-include "newtonsoft.json.schema.lua"
-group ""
--- rewrite/src/host/renderer.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-void Renderer_WriteColoredChar(char character, int32_t color);
-void Renderer_ClearNativeBuffer();
-void Renderer_Present();
-void Renderer_DisableCursor();
