70564677919e85930b5cc4ab43b3032388c61760
Cosmodash <theo.github@bedafamily.com>
2021-06-03 16:18:58 -0700

    add lance and axe skills

---
+++ src/FEEngine/Skills/AxeSkills.cs
@@ -0,0 +1,32 @@
+﻿using FEEngine.Classes;
+
+namespace FEEngine.Skills
+{
+    /// <summary>
+    /// Grants Hit/Avo +20 when using an axe against lance users.
+    /// </summary>
+    public class Lancebreaker : BreakerSkillBase
+    {
+        public Lancebreaker() : base(WeaponType.Axe, WeaponType.Lance) { }
+        public override Unit.UnitStats StatBoosts => ClassUtil.CreateStatBoosts();
+        public override string FriendlyName => nameof(Lancebreaker);
+    }
+    /// <summary>
+    /// Grants Crit +10 when using an axe.
+    /// </summary>
+    public class AxeCritPlus10 : CritPlus10Base
+    {
+        public AxeCritPlus10() : base(WeaponType.Axe) { }
+        public override Unit.UnitStats StatBoosts => ClassUtil.CreateStatBoosts();
+        public override string FriendlyName => "Axe Critical +10";
+    }
+    /// <summary>
+    /// Grants Atk +5 when using an axe.
+    /// </summary>
+    public class Axefaire : FaireSkillBase
+    {
+        public Axefaire() : base(WeaponType.Axe) { }
+        public override Unit.UnitStats StatBoosts => ClassUtil.CreateStatBoosts();
+        public override string FriendlyName => nameof(Axefaire);
+    }
+}
