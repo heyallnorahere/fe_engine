f78d1b731b06766897465f4443638ff0e4c45a87
yodasoda1219 <theo.github@bedafamily.com>
2022-02-13 12:46:58 -0800

    setup assemblies

---
+++ FEEngine/Engine.cs
@@ -0,0 +1,54 @@
+﻿/*
+   Copyright 2022 Nora Beda
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using FEEngine.Internal;
+using System.Collections.Generic;
+
+namespace FEEngine
+{
+    public static class Engine
+    {
+        static Engine()
+        {
+            mFactories = new Dictionary<string, IFactory>();
+            RegisterFactory<DefaultFactory>("Default");
+        }
+
+        public static bool RegisterFactory<T>(string name) where T : IFactory, new()
+        {
+            if (mFactories.ContainsKey(name))
+            {
+                return false;
+            }
+
+            mFactories.Add(name, new T());
+            return true;
+        }
+
+        public static IFactory? GetFactory(string name = "Default")
+        {
+            IFactory? factory = null;
+            if (mFactories.ContainsKey(name))
+            {
+                factory = mFactories[name];
+            }
+
+            return factory;
+        }
+
+        private static readonly Dictionary<string, IFactory> mFactories;
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,4 @@ This project aims to recreate the Fire Emblem: Three Houses game engine. All gam
 Fire Emblem is a tactical role-playing game developed by Intelligent Systems and published by Nintendo. The premise of each map is that you (the player) have a set of units (your army) and need to defeat the enemy's army, controlled by the game's AI. Each unit has a set amount of HP, and once that HP count reaches zero, they die, and once you lose a unit, it's gone from later maps.
 
 ## Building
-Simply install [.NET Core](https://dotnet.microsoft.com/download) and run `dotnet build` within the root directory of this repository.
+Simply install [.NET Core](https://dotnet.microsoft.com/download) and run `dotnet build -c Release` within the root directory of this repository.
